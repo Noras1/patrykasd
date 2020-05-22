4ef0eca47a4db8977d5c3a86a211ccad4f2dcf73
Noras1 <62107063+Noras1@users.noreply.github.com>
2020-05-22 09:02:42 +0200

    Add files via upload

---
+++ json.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+int main
+for(int i=0; i<5; i++){
+    cout << "Daj liczbe: ";
+      cin>>liczba;
+	if(liczba % 2 == 1){
+	   cout << "{\n \"nieparzysta"\: " << liczba" << "\n},n"	
+	}
+}
+	return 0;
+}
